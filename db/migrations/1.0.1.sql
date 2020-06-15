e458aadeddd4fd6de570892f869b3fd2a965aa59
Joe Balan <joebalan7@gmail.com>
2020-06-15 23:06:53 +0530

    Migration 1.0.1 added
    This migration is for db versioning

---
+++ db/migrations/1.0.1.sql
@@ -0,0 +1,8 @@
+# DB VERSION CREATED FOR TRACKING THE SCHEMA CHANGES
+
+CREATE TABLE `db_version` (  
+  `version` VARCHAR(10) NOT NULL
+);
+
+
+INSERT INTO `db_version` (`version`) VALUES ('1.0.1'); 
