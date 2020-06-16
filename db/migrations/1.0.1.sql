8dff512638f125798c100a996813e19353b27119
Joe Balan <joebalan7@gmail.com>
2020-06-16 20:47:09 +0530

    DB Versions merged into single file

--- db/migrations/1.0.1.sql
+++ db/migrations/1.0.1.sql
@@ -6,3 +6,15 @@ CREATE TABLE `db_version` (
 
 
 INSERT INTO `db_version` (`version`) VALUES ('1.0.1'); 
+
+
+
+
+# "prescription" table column "duration" updated nullable & default is NULL
+ALTER TABLE `prescription` CHANGE `duration` `duration` INT(11) DEFAULT NULL NULL; 
+
+# Migrate previous values from 0 to NULL
+update `prescription` set `duration` = NULL where `duration` = 0;
+
+
+update `db_version` set `version` = '1.0.1';
--- db/migrations/1.0.2.sql
+++
@@ -1,5 +0,0 @@
-# "prescription" table column "duration" updated nullable & default is NULL
-ALTER TABLE `prescription` CHANGE `duration` `duration` INT(11) DEFAULT NULL NULL; 
-
-# Migrate previous values from 0 to NULL
-update `prescription` set `duration` = NULL where `duration` = 0;
