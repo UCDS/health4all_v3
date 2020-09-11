2a90b162e1ead28880656a0765faa4e71dc63db7
Gunaranjan <gunaranjan@yousee.in>
2020-09-11 21:50:11 +0530

    Create 1.0.guna.sql

---
+++ db/migrations/1.0.guna.sql
@@ -0,0 +1,2 @@
+#New user function to create appointments
+INSERT INTO `user_function` (`user_function_id`, `user_function`, `user_function_display`, `description`) VALUES (NULL, 'create_appointment', 'Create Appointment', ''); 
