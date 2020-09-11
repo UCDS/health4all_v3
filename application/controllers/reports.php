43398a45d02435900306b58f742408ebdc98c056
Gunaranjan <gunaranjan@yousee.in>
2020-09-11 22:39:12 +0530

    Update reports.php

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -235,6 +235,50 @@ class Reports extends CI_Controller {
 		
 	}	
 	
+	public function create_appointment($department=0,$unit=0,$area=0,$gender=0,$from_age=0,$to_age=0,$from_date=0,$to_date=0)
+	{
+		if($this->session->userdata('logged_in')){
+		$this->data['userdata']=$this->session->userdata('logged_in');
+		$access=0;
+		foreach($this->data['functions'] as $function){
+			if($function->user_function=="create_appointment"){
+				$access=1;
+			}
+		}
+		if($access==1){
+		if($from_date == 0 && $to_date==0) {$from_date=date("Y-m-d");$to_date=$from_date;}
+		$this->data['title']="Create Appointment";
+		$this->data['all_departments']=$this->staff_model->get_department();
+		$this->data['units']=$this->staff_model->get_unit();
+		$this->data['areas']=$this->staff_model->get_area();
+		$this->data['visit_names']=$this->staff_model->get_visit_name();
+		$this->load->view('templates/header',$this->data);
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->data['report']=$this->reports_model->get_appointment($department,$unit,$area,$from_age,$to_age,$from_date,$to_date);
+		$this->form_validation->set_rules('from_date', 'From Date',
+		'trim|required|xss_clean');
+	    $this->form_validation->set_rules('to_date', 'To Date', 
+	    'trim|required|xss_clean');
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->load->view('pages/create_appointment',$this->data);
+		}
+		else{
+			$this->load->view('pages/create_appointment',$this->data);
+		}
+		$this->load->view('templates/footer');
+		}
+		else{
+		show_404();
+		}
+		}
+		else{
+		show_404();
+		}
+		
+	}
+	
 	public function ip_detail($department=-1,$unit=0,$area=0,$gender=0,$from_age=0,$to_age=0,$from_date=0,$to_date=0,$visit_name=-1)
 	{
 		if($this->session->userdata('logged_in')){
