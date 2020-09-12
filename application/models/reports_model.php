6b8159f8867628596ad46546206526363464e59a
Gunaranjan <gunaranjan@yousee.in>
2020-09-12 21:20:03 +0530

    Update reports_model.php

--- application/models/reports_model.php
+++ application/models/reports_model.php
@@ -532,7 +532,12 @@ function get_op_detail_with_idproof(){
 		$this->db->select("p.patient_id, p.address, hosp_file_no, pv.visit_id, CONCAT(IF(p.first_name=NULL,'',p.first_name),' ',IF(p.last_name=NULL,'',p.last_name)) name,
 		p.gender, IF(p.gender='F' AND (father_name IS NULL OR father_name = ''),spouse_name, father_name) parent_spouse, age_years, age_months, age_days,
 		p.place, p.phone, department, admit_date, admit_time, CONCAT(doctor.first_name, ' ', doctor.last_name) as doctor, 
-		CONCAT(volunteer.first_name, ' ', volunteer.last_name) as volunteer",false);
+		CONCAT(volunteer.first_name, ' ', volunteer.last_name) as volunteer,
+		IF(pv.signed_consultation=0, CONCAT(appointment_with.first_name, ' ', appointment_with.last_name), '') as appointment_with,
+		IF(pv.signed_consultation=0, DATE(appointment_time), '') as appointment_date,
+		IF(pv.signed_consultation=0, TIME(appointment_time), '') as appointment_time,
+		IF(pv.signed_consultation=0, CONCAT(appointment_update_by.first_name, ' ', appointment_update_by.last_name,' ',appointment_update_time), '') as appointment_update_by_time,
+		IF(pv.signed_consultation=0, '','') as button",false);
 		 $this->db->from('patient_visit as pv')
 		 ->join('patient as p','pv.patient_id=p.patient_id')
 		 ->join('department','pv.department_id=department.department_id','left')
@@ -540,11 +545,16 @@ function get_op_detail_with_idproof(){
 		 ->join('area','pv.area=area.area_id','left')
 		 ->join('hospital','pv.hospital_id=hospital.hospital_id','left')
 		 ->join('staff as doctor','pv.signed_consultation=doctor.staff_id','left')
+		 ->join('staff as appointment_with','pv.appointment_with=appointment_with.staff_id','left')
+		 ->join('staff as appointment_update_by','pv.appointment_update_by=appointment_update_by.staff_id','left')	 
 		 ->join('user as volunteer_user','p.insert_by_user_id = volunteer_user.user_id','left')
 		 ->join('staff as volunteer','volunteer_user.staff_id=volunteer.staff_id','left')		
 		 ->where('pv.hospital_id',$hospital['hospital_id'])
 		 ->where('visit_type','OP')
-		 ->where("(admit_date BETWEEN '$from_date' AND '$to_date')");
+		 ->where("(admit_date BETWEEN '$from_date' AND '$to_date')"); 
+		 ->order_by('admit_date','ASC');
+		 ->order_by('admit_time','ASC');
+			
 		$resource=$this->db->get();
 		return $resource->result();
 	}
