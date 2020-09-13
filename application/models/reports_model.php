98b8307a6c1d77f63476374ffc318e34e15cb956
Gunaranjan <gunaranjan@yousee.in>
2020-09-13 15:23:27 +0530

    Update reports_model.php

--- application/models/reports_model.php
+++ application/models/reports_model.php
@@ -538,7 +538,8 @@ function get_op_detail_with_idproof(){
 		IF(pv.signed_consultation=0, DATE(appointment_time), '') as appointment_date,
 		IF(pv.signed_consultation=0, TIME(appointment_time), '') as appointment_time,
 		IF(pv.signed_consultation=0, CONCAT(appointment_update_by.first_name, ' ', appointment_update_by.last_name),'') as appointment_update_by,
-		appointment_update_time, pv.signed_consultation as signed",false);
+		IF(pv.signed_consultation=0, appointment_update_time,'') as appointment_update_time,  
+		pv.signed_consultation as signed",false);
 		 $this->db->from('patient_visit as pv')
 		 ->join('patient as p','pv.patient_id=p.patient_id')
 		 ->join('department','pv.department_id=department.department_id','left')
