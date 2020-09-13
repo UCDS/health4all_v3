cef3469d00de99acb9344eb0d75ddb13d2c42cb4
Gunaranjan <gunaranjan@yousee.in>
2020-09-14 00:56:03 +0530

    Update create_appointment.php

--- application/views/pages/create_appointment.php
+++ application/views/pages/create_appointment.php
@@ -69,12 +69,11 @@ $(document).ready(function(){$("#from_date").datepicker({
 </script>
 <script type="text/javascript">
         $(document).ready(function(){
-			// find the input fields and apply the time select to them.
-           $('#from_time').ptTimeSelect();
-			$('#to_time').ptTimeSelect();
+	// find the input fields and apply the time select to them.
+        $('#from_time').ptTimeSelect();
+	$('#to_time').ptTimeSelect();
         });
-		
-    </script>
+</script>
 	<?php 
 	$from_date=0;$to_date=0;
 	if($this->input->post('from_date')) $from_date=date("Y-m-d",strtotime($this->input->post('from_date'))); else $from_date = date("Y-m-d");
@@ -86,58 +85,56 @@ $(document).ready(function(){$("#from_date").datepicker({
 	<div class="row">
 		<h4>Create Appointment</h4>	
 		<?php echo form_open("reports/create_appointment",array('role'=>'form','class'=>'form-custom')); ?> 
-					From Date : <input class="form-control" style = "background-color:#EEEEEE" type="text" value="<?php echo date("d-M-Y",strtotime($from_date)); ?>" name="from_date" id="from_date" size="15" />
-					To Date : <input class="form-control" type="text" style = "background-color:#EEEEEE" value="<?php echo date("d-M-Y",strtotime($to_date)); ?>" name="to_date" id="to_date" size="15" />
+			From Date : <input class="form-control" style = "background-color:#EEEEEE" type="text" value="<?php echo date("d-M-Y",strtotime($from_date)); ?>" name="from_date" id="from_date" size="15" />
+			To Date : <input class="form-control" type="text" style = "background-color:#EEEEEE" value="<?php echo date("d-M-Y",strtotime($to_date)); ?>" name="to_date" id="to_date" size="15" />
 	                From Time:<input  class="form-control" style = "background-color:#EEEEEE" type="text" value="<?php echo date("h:i A",strtotime($from_time)); ?>" name="from_time" id="from_time" size="7px"/>
-                   To Time:<input class="form-control" style = "background-color:#EEEEEE" type="text" value="<?php echo date("h:i A",strtotime($to_time)); ?>" name="to_time" id="to_time" size="7px"/>
-					
-					<select name="department" id="department" class="form-control">
-					<option value="">Department</option>
-					<?php 
-					foreach($all_departments as $dept){
-						echo "<option value='".$dept->department_id."'";
-						if($this->input->post('department') && $this->input->post('department') == $dept->department_id) echo " selected ";
-						echo ">".$dept->department."</option>";
-					}
-					?>
-					</select>
-					<select name="unit" id="unit" class="form-control" >
-					<option value="">Unit</option>
-					<?php 
-					foreach($units as $unit){
-						echo "<option value='".$unit->unit_id."' class='".$unit->department_id."'";
-						if($this->input->post('unit') && $this->input->post('unit') == $unit->unit_id) echo " selected ";
-						echo ">".$unit->unit_name."</option>";
-					}
-					?>
-					</select>
-					<select name="area" id="area" class="form-control" >
-					<option value="">Area</option>
-					<?php 
-					foreach($areas as $area){
-						echo "<option value='".$area->area_id."' class='".$area->department_id."'";
-						if($this->input->post('area') && $this->input->post('area') == $area->area_id) echo " selected ";
-						echo ">".$area->area_name."</option>";
-					}
-					?>
-					</select>
-					<select name="visit_name" id="visit_name" class="form-control" >
-					<option value="">Visit Type</option>
-					<?php 
-					foreach($visit_names as $v){
-						echo "<option value='".$v->visit_name_id."'";
-						if($this->input->post('visit_name') && $this->input->post('visit_name') == $v->visit_name_id) echo " selected ";
-						echo ">".$v->visit_name."</option>";
-					}
-					?>
-					</select>
-					<input class="btn btn-sm btn-primary" type="submit" value="Submit" />
+                   	To Time:<input class="form-control" style = "background-color:#EEEEEE" type="text" value="<?php echo date("h:i A",strtotime($to_time)); ?>" name="to_time" id="to_time" size="7px"/>
+			<select name="department" id="department" class="form-control">
+				<option value="">Department</option>
+				<?php 
+				foreach($all_departments as $dept){
+				echo "<option value='".$dept->department_id."'";
+				if($this->input->post('department') && $this->input->post('department') == $dept->department_id) echo " selected ";
+				echo ">".$dept->department."</option>";
+				}
+				?>
+			</select>
+			<select name="unit" id="unit" class="form-control" >
+				<option value="">Unit</option>
+				<?php 
+				foreach($units as $unit){
+				echo "<option value='".$unit->unit_id."' class='".$unit->department_id."'";
+				if($this->input->post('unit') && $this->input->post('unit') == $unit->unit_id) echo " selected ";
+				echo ">".$unit->unit_name."</option>";
+				}
+				?>
+			</select>
+			<select name="area" id="area" class="form-control" >
+				<option value="">Area</option>
+				<?php 
+				foreach($areas as $area){
+				echo "<option value='".$area->area_id."' class='".$area->department_id."'";
+				if($this->input->post('area') && $this->input->post('area') == $area->area_id) echo " selected ";
+				echo ">".$area->area_name."</option>";
+				}
+				?>
+			</select>
+			<select name="visit_name" id="visit_name" class="form-control" >
+				<option value="">Visit Type</option>
+				<?php 
+				foreach($visit_names as $v){
+				echo "<option value='".$v->visit_name_id."'";
+				if($this->input->post('visit_name') && $this->input->post('visit_name') == $v->visit_name_id) echo " selected ";
+				echo ">".$v->visit_name."</option>";
+				}
+				?>
+			</select>
+			<input class="btn btn-sm btn-primary" type="submit" value="Submit" />
 		</form>
 	<br />
 	<?php if(isset($report) && count($report)>0){ ?>
-	
 		<button type="button" class="btn btn-default btn-md print">
-		  <span class="glyphicon glyphicon-print"></span> Print
+		<span class="glyphicon glyphicon-print"></span> Print
 		</button>
 	<table class="table table-bordered table-striped" id="table-sort">
 	<thead>
@@ -164,7 +161,6 @@ $(document).ready(function(){$("#from_date").datepicker({
 	</thead>
 	<tbody>
 	<?php 
-	$total_count=0;
 	$sno=1;
 	foreach($report as $s){
 		$age="";
@@ -206,53 +202,56 @@ $(document).ready(function(){$("#from_date").datepicker({
 		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal_' . $sno .'">Update</button>
 		'; }?></td>
 	</tr>
-	<?php
-	$total_count++;
-	}
-	?>
-	<tr>
-		<th>Total </th>
-		<th ><?php echo $total_count;?></th>
-	</tr>
+	<?php }	?>
 	</tbody>
-	</table>
-i can 		
+	</table>	
 	<?php } else { ?>
 	No patient registrations on the given date.
 	<?php } ?>
 	</div>	
 
-	<?php if(isset($report) && count($report)>0){ ?>
-		<?php $sno=1;
-		foreach($report as $s){
-		$age="";
-		if(!!$s->age_years) $age.=$s->age_years."Y ";
-		if(!!$s->age_months) $age.=$s->age_months."M ";
-		if(!!$s->age_days) $age.=$s->age_days."D ";
-		if($s->age_days==0 && $s->age_months==0 && $s->age_years==0) $age.="0D"; ?>
-		<div class="modal fade" id="myModal_<?php echo $sno; ?>" role="dialog">
-		    <div class="modal-dialog">
-		      <!-- Modal content-->
-		      <div class="modal-content">
-			<div class="modal-header bg-primary text-white">
-			  <button type="button" class="close" data-dismiss="modal">&times;</button>
-			  <h4 class="modal-title">Update Appointment</h4>
-			</div>
-			<div class="modal-body">
+<?php if(isset($report) && count($report)>0){ ?>
+<?php $sno=1;
+	foreach($report as $s){
+	$age="";
+	if(!!$s->age_years) $age.=$s->age_years."Y ";
+	if(!!$s->age_months) $age.=$s->age_months."M ";
+	if(!!$s->age_days) $age.=$s->age_days."D ";
+	if($s->age_days==0 && $s->age_months==0 && $s->age_years==0) $age.="0D"; ?>
+		
+<div class="modal fade" id="myModal_<?php echo $sno; ?>" role="dialog">
+	<div class="modal-dialog">
+	<!-- Modal content-->
+	<div class="modal-content">
+		<div class="modal-header bg-primary text-white">
+			<button type="button" class="close" data-dismiss="modal">&times;</button>
+			<h4 class="modal-title">Update Appointment</h4>
+		</div>
+		<div class="modal-body">
 			<div>
-			<p><span><b>Patient ID:</b> <?php echo $s->patient_id;?>,&nbsp;</span><span><b>OP#:</b> <?php echo $s->hosp_file_no;?>,&nbsp;</span>
-			<span><b>Date:</b> <?php echo date("j M Y", strtotime("$s->admit_date"));?>&nbsp;<?php echo date("h:i A.", strtotime("$s->admit_time"));?>,&nbsp;</span></p>
-			<p class="bg-primary text-white"><span><b>Name:</b> <?php echo $s->name;?>,&nbsp;<?php echo $s->gender;?>&nbsp;/&nbsp;<?php echo $age;?>, &nbsp;<b>Relative:</b> <?php echo $s->parent_spouse;?>,&nbsp;
-			<span><b>Address:</b> <?php if(!!$s->address && !!$s->place) echo $s->address.", ".$s->place; else echo $s->address." ".$s->place;?>,&nbsp;</span>
-			<span><b>Ph:</b> <?php echo $s->phone;?>, &nbsp;</span></p>
-			<p><span>OP Created By: <?php echo $s->volunteer;?>,&nbsp;</span>
-			<span>Doctor Consulted: <?php echo $s->doctor;?></span></p>	
+				<p>
+				<span><b>Patient ID:</b> <?php echo $s->patient_id;?>,&nbsp;</span>
+				<span><b>OP#:</b> <?php echo $s->hosp_file_no;?>,&nbsp;</span>
+				<span><b>Date:</b> <?php echo date("j M Y", strtotime("$s->admit_date"));?>&nbsp;
+				<?php echo date("h:i A.", strtotime("$s->admit_time"));?>,&nbsp;</span>
+				</p>
+				<p class="bg-primary text-white">
+				<span><b>Name:</b> <?php echo $s->name;?>,&nbsp;<?php echo $s->gender;?>&nbsp;/&nbsp;
+				<?php echo $age;?>, &nbsp;<b>Relative:</b> <?php echo $s->parent_spouse;?>,&nbsp;</span>
+				<span><b>Address:</b> <?php if(!!$s->address && !!$s->place) echo $s->address.", ".$s->place; else echo $s->address." ".$s->place;?>,&nbsp;</span>
+				<span><b>Ph:</b> <?php echo $s->phone;?>, &nbsp;</span>
+				</p>
+				<p>
+				<span>OP Created By: <?php echo $s->volunteer;?>,&nbsp;</span>
+				<span>Doctor Consulted: <?php echo $s->doctor;?></span>
+				</p>	
 			</div>	
+			
 			<form action="/action_page.php">
 
-			  <div class="form-group">
-			    <label for="department">Department:</label>
-			    <select name="department" id="department" class="form-control">
+			<div class="form-group">
+				<label for="department">Department:</label>
+				<select name="department" id="department" class="form-control">
 					<option value="">Department</option>
 					<?php 
 					foreach($all_departments as $dept){
@@ -262,35 +261,36 @@ i can
 					}
 					?>
 				</select>
-			  </div>
-			  <div class="form-group">
-			    <label for="appointment_with">Appointment With:</label>
-			    <?php var_dump($helpline_doctor) ?>
-				  <select class="form-control" id="sel1">
-				    <option>1</option>
-				    <option>2</option>
-				    <option>3</option>
-				    <option>4</option>
-			    </select>
-			  </div>
-			  <div class="form-group">
-			    <label for="appointment_time">Appointment Date-Time:</label>
-			    <input type="text" class="form-control" id="pwd">
-			  </div>
-			  <div class="form-group">
-			    <label for="summary_sent_time">Summary Sent Date-Time:</label>
-			    <input type="text" class="form-control" id="pwd">
-			  </div>
-				
-			  <button type="submit" class="btn btn-default">Submit</button>
+			</div>
+			<div class="form-group">
+				<label for="appointment_with">Appointment With:</label>
+				<?php var_dump($helpline_doctor) ?>
+				<select class="form-control" id="sel1">
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="appointment_time">Appointment Date-Time:</label>
+				<input type="text" class="form-control" id="pwd">
+			</div>
+			<div class="form-group">
+				<label for="summary_sent_time">Summary Sent Date-Time:</label>
+				<input type="text" class="form-control" id="pwd">
+			</div>
+			
+			<button type="submit" class="btn btn-default">Submit</button>
 			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			
 			</form> 
-			</div>
-			</div>
+		</div>
+	</div>
 
-		    </div>
-		  </div>
-		<?php $sno++;}?>
-	<?php }?>
-  <!-- Modal -->
+	</div>
+</div>
+<?php $sno++;}?>
+<?php }?>
+ <!-- Modal -->
   
