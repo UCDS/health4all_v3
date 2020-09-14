c76d14d06df27d99ece5edacf1d577ede0d25015
Gunaranjan <gunaranjan@yousee.in>
2020-09-14 12:22:03 +0530

    Update reports_model.php

--- application/models/reports_model.php
+++ application/models/reports_model.php
@@ -483,9 +483,11 @@ function get_op_detail_with_idproof(){
 		 $this->db->from('staff')
 		 ->join('department','staff.department_id=department.department_id','left') 
 		 ->join('hospital','department.hospital_id=hospital.hospital_id','left')
-		 ->join('helpline_receiver','hospital.helpline_id=helpline_receiver.helpline_id','left') 
+		 ->join('helpline_receiver','hospital.helpline_id=helpline_receiver.helpline_id','left')
+		 ->join('user','user.user_id=helpline_receiver.user_id','left')	 
 		 ->where('hospital.hospital_id',$hospital['hospital_id'])
-		 ->where('helpline_receiver.doctor',1);	 
+		 ->where('helpline_receiver.doctor',1);
+		 ->where('staff.user_id','user.user_id');
 		 $this->db->order_by('department','ASC');
 		 $this->db->order_by('helpline_doctor','ASC');
 		
