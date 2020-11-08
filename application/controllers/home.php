027b11c6c2ecccf9bfbf9b9aa58d1934152b1843
Suresh-rsa <74096114+Suresh-rsa@users.noreply.github.com>
2020-11-08 20:24:32 +0530

    Include home.php

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,6 +4,7 @@ class Home extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->model('staff_model');
+		$this->load->model('masters_model');
 		if($this->session->userdata('logged_in')){
 			$userdata = $this->session->userdata('logged_in');
 			$user_id = $userdata['user_id'];
@@ -12,7 +13,8 @@ class Home extends CI_Controller {
 			$this->data['departments']=$this->staff_model->user_department($user_id);
 			$this->data['op_forms']=$this->staff_model->get_forms("OP");
 			$this->data['ip_forms']=$this->staff_model->get_forms("IP");
-		}
+			}
+			$this->data['defaultsConfigs']=$this->masters_model->get_data("defaults");
 	}
 	public function index(){
 		$this->data['title']="Home";
