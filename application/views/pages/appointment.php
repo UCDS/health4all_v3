57b69ab16ee39df0c0736abdeb87709f9e0a7dc6
manojkumarmanusai <manusai.manoj@gmail.com>
2022-01-09 18:19:27 +0530

    Checking unix format error

--- application/views/pages/appointment.php
+++ application/views/pages/appointment.php
@@ -340,7 +340,7 @@ display: inline-grid;
 			Manual ID : <input type="text" class="form-custom form-control" name="manualid" placeholder="Manual ID" id="manualid" value="<?php if($this->input->post('manualid')) { echo $this->input->post('manualid'); } ?>"  /> 
 			  H4All ID : <input type="number" class="form-custom form-control" placeholder="Health4All ID" name="patientid" id="h4allid" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" value="<?php if($this->input->post('patientid')) { echo $this->input->post('patientid'); } ?>"   />  			
 			  Rows per page : <input type="number" class="rows_per_page form-custom form-control" name="rows_per_page" id="rows_per_page" min=<?php echo $lower_rowsperpage; ?> max= <?php echo $upper_rowsperpage; ?> step="1" value= <?php if($this->input->post('rows_per_page')) { echo $this->input->post('rows_per_page'); }else{echo $rowsperpage;}  ?> onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" /> 
-			  <input type="checkbox" id="myactivity" name="myactivity" value="1" <?php if($this->input->post('myactivity')) echo "checked"; ?>/>
+			<input type="checkbox" id="myactivity" name="myactivity" value="1" <?php if($this->input->post('myactivity')) echo "checked"; ?>/>
   			<label for="myactivity" style="font-weight:normal">Show only my activity</label>
 			<input class="btn btn-sm btn-primary" type="submit" value="Submit" />
 		</form>
