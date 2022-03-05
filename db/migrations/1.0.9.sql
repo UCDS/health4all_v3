40a43e92a14dd84015a4980c19905a08dfd2cd68
YouSee <gunaranjan@yousee.one>
2022-03-05 21:16:19 +0530

    Delete 1.0.9.sql

--- db/migrations/1.0.9.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `user` ADD `active` TINYINT(1) NOT NULL DEFAULT '1'; 
-
-Insert into defaults values ('Login_Status_Deactive','Account Deactivated','','Text','','','','Account has been deactivated. Please contact admin.');
-
