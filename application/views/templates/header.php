9f949c8b408528aac5fc28dcb2f64dedea680679
manojkumarmanusai <manusai.manoj@gmail.com>
2022-03-18 00:46:00 +0530

    Added appointment status filter and removed follow up report

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -261,7 +261,7 @@
 					$f->user_function=="OP Detail" || $f->user_function=="IP Detail" ||
 					$f->user_function=="Diagnostics - Detail" || $f->user_function=="Diagnostics - Summary" ||
 					($f->user_function == "Sanitation Evaluation" && $f->view==1) ||
-					$f->user_function == "Bloodbank" || $f->user_function == "Outcome Summary" || $f->user_function == "Helpline Reports"|| $f->user_function == "follow_up_report" || $f->user_function =="appointment_status"){ ?>
+					$f->user_function == "Bloodbank" || $f->user_function == "Outcome Summary" || $f->user_function == "Helpline Reports" || $f->user_function =="appointment_status"){ ?>
 					<li class="dropdown  <?php if(preg_match("^".base_url()."reports^",current_url())){ echo "active";}?>">
 						<a href="#" class="dropdown-toggle js-activated" data-toggle="dropdown"><i class="fa fa-line-chart"></i> Reports <b class="caret"></b></a>
 						<ul class="dropdown-menu">
@@ -363,10 +363,6 @@
                         <?php	}
 			if($f->user_function=="doctor_patient_list"){ ?>
 				<li><a href="<?php echo base_url()."reports/doctor_patient_list";?>">Doctor Patient List</a></li>	
-			<?php	}
-			if($f->user_function=="follow_up_report"){ ?>
-				<li><a href="<?php echo base_url()."report/get/follow_up_report";?>">Follow Up Report</a></li>
-						
 			<?php	}
 			if($f->user_function=="IP Detail"){ ?>
 						<li><a href="<?php echo base_url()."reports/ip_detail";?>">IP Detail</a></li>
