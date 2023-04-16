b2fcc91343d12b50e6e271cdcb867d9cf1abe0e1
Shruthi S M - E006 - Code Ctrl Solutions Private Limited <122970329+shruthism-e006-codectrlsolutions@users.noreply.github.com>
2023-04-16 17:24:23 +0530

    Update 1.0.9.sql

--- db/migrations/1.0.9.sql
+++ db/migrations/1.0.9.sql
@@ -33,6 +33,99 @@ ALTER TABLE `sms_template` ADD `default_sms` TINYINT(1) DEFAULT 0;
 
 ALTER TABLE hospital ADD auto_ip_number tinyint(1) DEFAULT 0;
 
+CREATE TABLE `patient_followup` (
+  `patient_followup_id` int(11) NOT NULL,
+  `patient_id` int(8) NOT NULL,
+  `hospital_id` int(8) NOT NULL,
+  `longitude` varchar(255) NOT NULL,
+  `latitude` varchar(255) NOT NULL,
+  `life_status` tinyint(4) NOT NULL COMMENT '''0'' - death ''1'' - Alive',
+  `status_date` datetime NOT NULL,
+  `icd_code` varchar(200) NOT NULL,
+  `diagnosis` varchar(200) NOT NULL,
+  `last_visit_type` varchar(60) NOT NULL,
+  `last_visit_date` datetime NOT NULL,
+  `priority_type_id` int(8) NOT NULL,
+  `route_primary_id` int(8) NOT NULL,
+  `route_secondary_id` int(8) NOT NULL,
+  `volunteer_id` int(8) NOT NULL,
+  `note` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `patient_followup`
+  ADD PRIMARY KEY (`patient_followup_id`),
+  ADD UNIQUE KEY `patient_id` (`patient_id`);
+  ALTER TABLE `patient_followup`
+  MODIFY `patient_followup_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+CREATE TABLE `priority_type` (
+  `priority_type_id` int(11) NOT NULL,
+  `hospital_id` int(8) NOT NULL,
+  `priority_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `priority_type`
+  ADD PRIMARY KEY (`priority_type_id`);
+  ALTER TABLE `priority_type`
+  MODIFY `priority_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+
+CREATE TABLE `route_primary` (
+  `route_primary_id` int(11) NOT NULL,
+  `hospital_id` int(8) NOT NULL,
+  `route_primary` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `route_primary`
+  ADD PRIMARY KEY (`route_primary_id`);
+  ALTER TABLE `route_primary`
+  MODIFY `route_primary_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+CREATE TABLE `route_secondary` (
+  `id` int(11) NOT NULL,
+  `hospital_id` int(8) NOT NULL,
+  `route_primary_id` int(8) NOT NULL,
+  `route_secondary` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `route_secondary`
+  ADD PRIMARY KEY (`id`);
+  ALTER TABLE `route_secondary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+CREATE TABLE `hospital` (
+  `hospital_id` int(3) NOT NULL,
+  `hospital_type_id` int(6) NOT NULL,
+  `hospital` varchar(200) NOT NULL,
+  `hospital_short_name` varchar(25) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `place` varchar(50) NOT NULL,
+  `village_town_id` int(6) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `district_id` int(3) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `map_link` varchar(500) NOT NULL,
+  `logo` varchar(200) NOT NULL,
+  `type1` varchar(50) NOT NULL COMMENT 'Private, Public, Non-Profit',
+  `type2` varchar(50) NOT NULL COMMENT 'State Govt., Central Govt.',
+  `type3` varchar(50) NOT NULL COMMENT 'Teaching, Non-Teaching',
+  `type4` varchar(50) NOT NULL COMMENT 'District, Area, CHC, PHC, Sub Centre',
+  `type5` varchar(50) NOT NULL COMMENT 'Urban, Rural',
+  `type6` varchar(50) NOT NULL COMMENT 'DME, VVP, DH',
+  `helpline_id` int(11) DEFAULT NULL,
+  `telehealth` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Determine whether hospital is running as telemedicine (1) or not ',
+  `auto_ip_number` tinyint(1) DEFAULT 0,
+  `print_layout_id` int(6) NOT NULL,
+  `a6_print_layout_id` int(6) NOT NULL,
+  `update_date_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_by_id` int(8) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='List of hospitals';
+
+ALTER TABLE `hospital`
+  ADD PRIMARY KEY (`hospital_id`),
+  ADD KEY `hospital_id` (`hospital_id`);
+COMMIT;
+
+
+
 ALTER TABLE `helpline_call` ADD INDEX `start_time_index` (`start_time`);
 
 ALTER TABLE `patient_visit` DROP INDEX `hospital_id`;
