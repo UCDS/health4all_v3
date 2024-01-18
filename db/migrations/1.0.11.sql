d54432c450b31d176edeb265b99e34644913059a
anilvng <anilmandanapu2@gmail.com>
2024-01-18 18:53:35 +0530

    modified sql 1.0.10 and deleted sql 1.0.11

--- db/migrations/1.0.11.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `patient_visit` CHANGE `outcome` `outcome` VARCHAR(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'Discharge,LAMA,Absconded,Death';
-
-
-INSERT INTO `user_function`(`user_function_id`, `user_function`, `user_function_display`, `description`) VALUES ('','issue_list','Issue List','issue list report')
-
-
-Table  -  user_function_link => make link_id ( column auto increment )
